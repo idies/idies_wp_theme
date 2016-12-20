57d4582ec4ed26659383a72b34529a27e3767789
bonbons0220 <bsouter@jhu.edu>
2016-12-20 16:21:10 -0500

    Fixed formating on knowledgebase archive to add obvious link to article

--- templates/content-knowledgebase.php
+++ templates/content-knowledgebase.php
@@ -3,9 +3,10 @@
 ?>
 <article <?php post_class( "panel panel-primary " ); ?> id="article-<?php the_id(); ?>">
   <header class="panel-heading">
-    <p class="h4 entry-title panel-title"><a href="knowledgebase/<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+    <p class="h4 entry-title panel-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
   </header>
   <div class="entry-summary panel-body">
 	<?php the_excerpt(); ?>
+	<p class="text-right"><a href="<?php the_permalink(); ?>"><em>Read Article</em><a></p>
   </div>
 </article>
