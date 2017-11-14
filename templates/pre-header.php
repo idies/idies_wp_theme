9a4d8790d4f0512c081070fbfc10d4c699745648
bonbons0220 <bsouter@jhu.edu>
2017-11-14 13:38:30 -0500

    updated google analytics - tag manager code

--- templates/pre-header.php
+++ templates/pre-header.php
@@ -5,7 +5,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M6F94N');</script>
+})(window,document,'script','dataLayer','GTM-PNZJNX');</script>
 <!-- End Google Tag Manager -->
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
